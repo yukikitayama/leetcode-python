bd95289e538a84873d00046858969d6d8891bbee
Yuki Kitayama <yk2797@columbia.edu>
2021-09-19 10:38:37 -0600

    solved 1525 by using collections.Counter and for loop with t: O(n) and s: O(26)

---
+++ 1525_g_number_of_good_ways_to_split_a_string.py
@@ -0,0 +1,49 @@
+from collections import Counter
+
+
+class Solution:
+    def numSplits(self, s: str) -> int:
+        # Empty counter is initialized
+        left_counter = Counter()
+        right_counter = Counter(s)
+
+        # print(f'left_counter: {left_counter}, right_counter: {right_counter}')
+
+        res = 0
+
+        for c in s:
+
+            left_counter[c] += 1
+            right_counter[c] -= 1
+
+            if right_counter[c] == 0:
+                del right_counter[c]
+
+            # print(f'c: {c}, left_counter: {left_counter}, right_counter: {right_counter}')
+
+            if len(left_counter) == len(right_counter):
+                res += 1
+
+        return res
+
+
+"""
+Use two counter dictionaries. One is initially empty and the other is counted for all the characters in s
+As we iterate each character in s, we increment empty counter and decrement the full counter.
+Important thing is that, when we have 0 in the initially full counter, need to delete the key and value,
+because eventually we will compare the number of keys in each dictionary to know how many distinct characters
+Distinct characters do not need to be the same. Just the number of distinc characters.
+
+Time complexity
+Let n be the length of s. O(n) to do for loop.
+
+Space complexity
+O(26) because we will only have lowercase English letter, so it's not n
+"""
+
+
+s = "aacaba"
+s = "abcd"
+# s = "aaaaa"
+s = "acbadbaada"
+print(Solution().numSplits(s))
