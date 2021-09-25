4c6dc52a471e586eb6e21f88469a79664da40fb4
Yuki Kitayama <yk2797@columbia.edu>
2021-09-25 11:21:54 -0600

    solved 119 again by memory efficient dynamic programming

---
+++ dynamic-programming/dp_119_pascals_triangle_ii_2.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        row = [1]
+
+        for i in range(rowIndex):
+            for j in range(i, 0, -1):
+                row[j] = row[j] + row[j - 1]
+            row.append(1)
+
+        return row
+
+
+"""
+Memory efficient dynamic progamming
+"""
+print(Solution().getRow(2))
+
