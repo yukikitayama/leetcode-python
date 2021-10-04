637ae7db59bc1aaa6671092b9245dd0296b31ee2
Yuki Kitayama <yk2797@columbia.edu>
2021-10-04 17:54:17 -0600

    tried 399 again for review with DFS

---
+++ 399_g_evaluate_division_4.py
@@ -0,0 +1,55 @@
+from typing import List
+from collections import defaultdict
+
+
+class Solution:
+    def __init__(self):
+        self.graph = None
+
+    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+        self.graph = defaultdict(defaultdict)
+        for i, equation in enumerate(equations):
+            self.graph[equation[0]][equation[1]] = values[i]
+            self.graph[equation[1]][equation[0]] = 1 / values[i]
+
+        answers = []
+
+        for query in queries:
+
+            if query[0] not in self.graph or query[1] not in self.graph:
+                answer = -1.0
+
+            elif query[0] == query[1]:
+                answer = 1.0
+
+            else:
+                answer = self.traverse(query[0], query[1], 1.0, set())
+
+            answers.append(answer)
+
+        return answers
+
+    def traverse(self, curr_vertex: str, dest_vertex: str, multiplier: float, visited: set) -> float:
+        visited.add(curr_vertex)
+        answer = -1.0
+
+        if dest_vertex in self.graph[curr_vertex]:
+            answer = self.graph[curr_vertex][dest_vertex] * multiplier
+
+        else:
+            for next_vertex, value in self.graph[curr_vertex].items():
+                if next_vertex in visited:
+                    continue
+                answer = self.traverse(next_vertex, dest_vertex, value * multiplier, visited)
+
+                if answer != -1:
+                    break
+
+        return answer
+
+
+equations = [["a","b"],["b","c"]]
+values = [2.0,3.0]
+queries = [["a","c"],["b","a"],["a","e"],["a","a"],["x","x"]]
+print(Solution().calcEquation(equations, values, queries))
+
