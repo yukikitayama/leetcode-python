027f8068fc5211362ef00ca0305d49128c188055
Yuki Kitayama <yk2797@columbia.edu>
2021-11-04 19:31:30 -0600

    solved 1093. median is not well

---
+++ probability-and-statistics/pas_1093_statistics_from_a_large_sample.py
@@ -0,0 +1,67 @@
+from typing import List
+import bisect
+
+
+class Solution:
+    def sampleStats(self, count: List[int]) -> List[float]:
+
+        for i, c in enumerate(count):
+            if c > 0:
+                minimum = i
+                break
+
+        for i, c in enumerate(count[::-1]):
+            if c > 0:
+                maximum = 255 - i
+                break
+
+        n = sum(count)
+        total = 0
+        for i, c in enumerate(count):
+            total += i * c
+        mean = total / n
+
+        max_count_so_far = 0
+        for i, c in enumerate(count):
+            if c > max_count_so_far:
+                max_count_so_far = c
+                mode = i
+
+        # Modify count as count[i] represents the number of integers up to i
+        for i in range(255):
+            count[i + 1] += count[i]
+        # print(f'count: {count}')
+        # When n is odd, (n - 1) / 2 becomes integer, and n / 2 becomes float .5
+        # When n is even, (n - 1) / 2 becomes float .5, and n / 2 becomes integer
+        # print(f'n: {n}')
+        # print(f'(n - 1) / 2: {(n - 1) / 2}')
+        # print(f'n / 2: {n / 2}')
+        # bisect gives us the index, but in this problem index represents integer
+        # bisect is equal to bisect_right
+        # gives us the index which comes after existing entries
+        median1 = bisect.bisect(count, (n - 1) / 2)
+        median2 = bisect.bisect(count, n / 2)
+        # When n is odd median1 is equal to median2
+        # print(f'median1: {median1}, median2: {median2}')
+        median = (median1 + median2) / 2.0
+
+        return [float(minimum), float(maximum), mean, median, float(mode)]
+
+
+count = [
+    0, 1, 3, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+]
+# count = [0,4,3,2,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+# count = [0,1,2,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+print(Solution().sampleStats(count))
+
+
+
+
