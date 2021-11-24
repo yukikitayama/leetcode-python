d6e2041ff4e1d951e5933933c474b9694ce7efb5
Yuki Kitayama <yk2797@columbia.edu>
2021-11-23 21:00:11 -0700

    solved 657 by checking each character in the for loop

---
+++ goldman-sachs/gs_657_robot_return_to_origin.py
@@ -0,0 +1,30 @@
+class Solution:
+    def judgeCircle(self, moves: str) -> bool:
+        x = 0
+        y = 0
+        for move in moves:
+            if move == 'U':
+                y += 1
+            elif move == 'D':
+                y -= 1
+            elif move == 'R':
+                x += 1
+            elif move == 'L':
+                x -= 1
+
+        return x == 0 and y == 0
+
+
+"""
+Complexity
+- Time is O(n)
+- Space is O(1)
+"""
+
+
+moves = "UD"
+moves = "LL"
+moves = "RRDD"
+moves = "LDRRLRUULR"  # False
+print(Solution().judgeCircle(moves))
+
