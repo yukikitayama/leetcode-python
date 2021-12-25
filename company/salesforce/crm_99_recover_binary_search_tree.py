aa6cc2f785f687ae0a81ae08d5b8281771e2de60
Yuki Kitayama <yk2797@columbia.edu>
2021-12-25 14:58:57 -0700

    solved 99 by inorder traversal

---
+++ company/salesforce/crm_99_recover_binary_search_tree.py
@@ -0,0 +1,47 @@
+"""
+- Keep prev (parent) and flag about left or right
+  - Compare curr (child) and prev depending on flag
+- Postorder traversal
+"""
+
+
+from typing import Optional, List, Tuple
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution2:
+    def recoverTree(self, root: Optional[TreeNode]) -> None:
+
+
+
+class Solution1:
+    def recoverTree(self, root: Optional[TreeNode]) -> None:
+        stack = []
+        x = y = prev = None
+
+        while stack or root:
+            while root:
+                stack.append(root)
+                root = root.left
+            root = stack.pop()
+            # current node (root) needs to be bigger than prev,
+            # but if not, need to swap
+            if prev and root.val < prev.val:
+                y = root
+                if x is None:
+                    x = prev
+                else:
+                    break
+            prev = root
+            root = root.right
+
+        # Swap
+        x.val, y.val = y.val, x.val
+
+
