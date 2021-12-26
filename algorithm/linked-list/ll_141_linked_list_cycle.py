278673e2da41cc8a4491e0d29d7309199e197d5e
Yuki Kitayama <yk2797@columbia.edu>
2021-12-26 11:44:51 -0700

    solved 141 by two pointers in linked list

---
+++ algorithm/linked-list/ll_141_linked_list_cycle.py
@@ -0,0 +1,34 @@
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        fast = head
+        slow = head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+            if slow == fast:
+                return True
+
+        return False
+
+
+if __name__ == '__main__':
+    head = ListNode(3)
+    head.next = ListNode(2)
+    head.next.next = ListNode(0)
+    head.next.next.next = ListNode(-4)
+    head.next.next.next.next = head.next
+
+    head = ListNode(1)
+
+    print(Solution().hasCycle(head))
