97dbfe1cc7b7ff9626ad429ea71be094dc3cc4bc
Yuki Kitayama <yk2797@columbia.edu>
2022-01-14 19:58:45 -0700

    solved 8 by deterministic finite automation

--- README.md
+++ README.md
@@ -652,6 +652,26 @@ taking O(N) every time in calling find().
 - [134. Gas Station](https://leetcode.com/problems/gas-station/)
 - [452. Minimum Number of Arrows to Burst Balloons](https://leetcode.com/problems/minimum-number-of-arrows-to-burst-balloons/)
 
+## Deterministic Finite Automation (DFA)
+
+- `State machine` reads some input and changes the states based on those inputs.
+- `Transition` is the change in a state.
+- `Finite state machine` is the state machine with a finite number of states.
+- `Deterministic finite automation (DFA)` is the finite state machine that either `accept` or `reject` a sequence of
+   states.
+  - e.g. Traffic light. "Green -> Yellow -> Red -> Green" are accepted each time in the sequence. But "Green -> Yellow
+    -> Red -> Yellow" is rejected when Red changes into Yellow.
+- There is only one path for the specific input from the current state to the next state.
+
+#### Problem
+
+- [10. Regular Expression Matching](https://leetcode.com/problems/regular-expression-matching/)
+- [44. Wildcard Matching](https://leetcode.com/problems/wildcard-matching/)
+- [65. Valid Number](https://leetcode.com/problems/valid-number/)
+- [520. Detect Capital](https://leetcode.com/problems/detect-capital/)
+- [890. Find and Replace Pattern](https://leetcode.com/problems/find-and-replace-pattern/)
+- [1018. Binary Prefix Divisible By 5](https://leetcode.com/problems/binary-prefix-divisible-by-5/)
+
 ## Hash table
 
 When you use modulo as hash function, the divisor should use prime number (e.g. 1999), because it can best avoid 
--- daily-challenge/daily_8_string_to_integer_atoi.py
+++ daily-challenge/daily_8_string_to_integer_atoi.py
@@ -1,4 +1,73 @@
+class StateMachine:
+    def __init__(self):
+        self.State = {'q0': 1, 'q1': 2, 'q2': 3, 'qd': 4}
+        self.INT_MAX = pow(2, 31) - 1
+        self.INT_MIN = -pow(2, 31)
+        # Super private variable accessible by
+        self.__current_state = self.State['q0']
+        self.__result = 0
+        self.__sign = 1
+
+    def to_state_q1(self, ch: chr) -> None:
+        self.__sign = -1 if (ch == '-') else 1
+        self.__current_state = self.State['q1']
+
+    def to_state_q2(self, digit: int) -> None:
+        self.__current_state = self.State['q2']
+        self.append_digit(digit)
+
+    def to_state_qd(self) -> None:
+        self.__current_state = self.State['qd']
+
+    def append_digit(self, digit: int) -> None:
+        if (self.__result > self.INT_MAX // 10) or (self.__result == self.INT_MAX // 10 and digit > self.INT_MAX % 10):
+            if self.__sign == 1:
+                self.__result = self.INT_MAX
+            else:
+                self.__result = self.INT_MIN
+                self.__sign = 1
+            self.to_state_qd()
+        else:
+            self.__result = (self.__result * 10) + digit
+
+    def transition(self, ch: chr) -> None:
+        if self.__current_state == self.State['q0']:
+            if ch == ' ':
+                # Stay in the same state
+                return
+            elif ch == '-' or ch == '+':
+                self.to_state_q1(ch)
+            elif ch.isdigit():
+                self.to_state_q2(int(ch))
+            else:
+                self.to_state_qd()
+
+        elif self.__current_state == self.State['q1'] or self.__current_state == self.State['q2']:
+            if ch.isdigit():
+                self.to_state_q2(int(ch))
+            else:
+                self.to_state_qd()
+
+    def get_integer(self) -> int:
+        return self.__sign * self.__result
+
+    def get_state(self) -> int:
+        return self.__current_state
+
+
 class Solution:
+    def myAtoi(self, s: str) -> int:
+        q = StateMachine()
+
+        for ch in s:
+            q.transition(ch)
+            if q.get_state() == q.State['qd']:
+                break
+
+        return q.get_integer()
+
+
+class Solution1:
     def myAtoi(self, s: str) -> int:
         sign = 1
         result = 0
