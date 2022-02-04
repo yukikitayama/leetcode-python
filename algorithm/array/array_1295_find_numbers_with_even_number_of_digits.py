fa13b035f57d5ab74002ae254f2bfcbaf76fb520
Yuki Kitayama <yk2797@columbia.edu>
2022-02-03 19:33:13 -0700

    solved 1295 by one pass

---
+++ algorithm/array/array_1295_find_numbers_with_even_number_of_digits.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+class Solution:
+    def findNumbers(self, nums: List[int]) -> int:
+        ans = 0
+        for num in nums:
+            if len(str(num)) % 2 == 0:
+                ans += 1
+        return ans
+
