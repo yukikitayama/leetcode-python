d0343860f4268be1c2eb973bcd65f98c6f80092b
Yuki Kitayama <yk2797@columbia.edu>
2022-05-09 07:48:56 -0700

    solved 1211 by avg, round, group by

---
+++ database/db_1211_queries_quality_and_percentage.sql
@@ -0,0 +1,20 @@
+# Write your MySQL query statement below
+# select
+#   query_name,
+#   round(sum(rating / position) / count(rating), 2) as quality,
+#   round(sum(if(rating < 3, 1, 0)) / count(rating) * 100, 2) as poor_query_percentage
+# from
+#   queries
+# group by
+#   query_name
+# ;
+
+select
+  query_name,
+  round(avg(rating / position), 2) as quality,
+  round(avg(rating < 3) * 100, 2) as poor_query_percentage
+from
+  queries
+group by
+  query_name
+;
