4a8f2fc53affcaf99cc0b2cae32f3787f15b50a3
Yuki Kitayama <yk2797@columbia.edu>
2022-05-10 10:20:18 -0700

    solved 1140 by case when

---
+++ database/db_1440_evaluate_boolean_expression.sql
@@ -0,0 +1,21 @@
+# Write your MySQL query statement below
+select
+  a.left_operand,
+  a.operator,
+  a.right_operand,
+  case
+    when a.operator = '<' and b.value < c.value then 'true'
+    when a.operator = '=' and b.value = c.value then 'true'
+    when a.operator = '>' and b.value > c.value then 'true'
+    else 'false'
+  end as value
+from
+  expressions as a
+left join
+  variables as b
+on
+  a.left_operand = b.name
+left join
+  variables as c
+on
+  a.right_operand = c.name
