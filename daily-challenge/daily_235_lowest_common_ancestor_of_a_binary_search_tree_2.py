77588bbb36a63d8ba498541a95edbab47cd2fbfe
Yuki Kitayama <yk2797@columbia.edu>
2022-08-12 15:05:51 -0700

    solved 235 by recursion in BST

---
+++ daily-challenge/daily_235_lowest_common_ancestor_of_a_binary_search_tree_2.py
@@ -0,0 +1,25 @@
+"""
+- Use property of BST
+- Recursion
+- If p and q are all in left or all in right, go to that direction
+"""
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+
+        if p.val < root.val and q.val < root.val:
+            return self.lowestCommonAncestor(root.left, p, q)
+
+        elif root.val < p.val and root.val < q.val:
+            return self.lowestCommonAncestor(root.right, p, q)
+
+        else:
+            return root
