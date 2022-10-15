f18f003343a1bcabb414c205be76f7b33bd1eab7
Yuki Kitayama <yk2797@columbia.edu>
2022-10-15 14:25:41 -0700

    solved 1531 by dp but didn't understand

---
+++ daily-challenge/daily_1531_string_compression_ii.py
@@ -0,0 +1,30 @@
+import functools
+
+
+class Solution:
+    def getLengthOfOptimalCompression(self, s: str, k: int) -> int:
+        n = len(s)
+
+        @functools.lru_cache(None)
+        def dp(idx, last_char, last_char_count, k):
+
+            if k < 0:
+                return float('inf')
+
+            if idx == n:
+                return 0
+
+            delete_char = dp(idx + 1, last_char, last_char_count, k - 1)
+
+            if s[idx] == last_char:
+                keep_char = dp(idx + 1, last_char, last_char_count + 1, k) + (last_char_count in [1, 9, 99])
+            else:
+                keep_char = dp(idx + 1, s[idx], 1, k) + 1
+
+            return min(keep_char, delete_char)
+
+        return dp(0, '', 0, k)
+
+
+if __name__ == '__main__':
+    pass
