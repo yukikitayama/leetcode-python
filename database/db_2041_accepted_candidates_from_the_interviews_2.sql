4ca038122d993e2e971c6be9dfa63e5926603fd3
Yuki Kitayama <yk2797@columbia.edu>
2022-11-22 09:43:16 -0800

    solved 2041 by group by having sum

---
+++ database/db_2041_accepted_candidates_from_the_interviews_2.sql
@@ -0,0 +1,15 @@
+select
+  distinct a.candidate_id
+from
+  candidates as a
+left join
+  rounds as b
+on
+  a.interview_id = b.interview_id
+where
+  a.years_of_exp >= 2
+group by
+  a.interview_id
+having
+  sum(b.score) > 15
+;
