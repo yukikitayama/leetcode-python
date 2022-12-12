f72ab1c7078da97b8ac6c1151100f7a824f9ac35
Yuki Kitayama <yk2797@columbia.edu>
2022-12-12 06:15:25 -0800

    solved 70 by dp array

---
+++ daily-challenge/daily_70_climbing_stairs_3.py
@@ -0,0 +1,22 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+
+        if n == 1:
+            return 1
+        elif n == 2:
+            return 2
+        else:
+            dp = [0] * n
+            dp[0] = 1
+            dp[1] = 2
+
+            for i in range(2, n):
+                dp[i] = dp[i - 1] + dp[i - 2]
+
+            return dp[-1]
+
+
+if __name__ == '__main__':
+    n = 3
+    n = 4
+    print(Solution().climbStairs(n))
