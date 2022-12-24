88887ba88b86624ff23323d3ae19809956a3afb6
Yuki Kitayama <yk2797@columbia.edu>
2022-12-24 11:22:10 -0800

    solved 790 by dp

---
+++ daily-challenge/daily_790_domino_and_tromino_tiling_2.py
@@ -0,0 +1,20 @@
+import functools
+
+
+class Solution:
+    def numTilings(self, n: int) -> int:
+        MOD = 1_000_000_007
+
+        @functools.cache
+        def p(n):
+            if n == 2:
+                return 1
+            return (p(n - 1) + f(n - 2)) % MOD
+
+        @functools.cache
+        def f(n):
+            if n <= 2:
+                return n
+            return (f(n - 1) + f(n - 2) + 2 * p(n - 1)) % MOD
+
+        return f(n)
