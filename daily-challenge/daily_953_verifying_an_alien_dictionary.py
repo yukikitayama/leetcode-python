c923cece786ccbc081f84a55d2b62aafaa4ce20e
Yuki Kitayama <yk2797@columbia.edu>
2023-02-02 17:14:40 +0900

    solved 953 by iteration

---
+++ daily-challenge/daily_953_verifying_an_alien_dictionary.py
@@ -0,0 +1,25 @@
+from typing import List
+
+
+class Solution:
+    def isAlienSorted(self, words: List[str], order: str) -> bool:
+
+        order_map = {}
+        for i, val in enumerate(order):
+            order_map[val] = i
+
+        for i in range(len(words) - 1):
+
+            for j in range(len(words[i])):
+
+                if j >= len(words[i + 1]):
+                    return False
+
+                if words[i][j] != words[i + 1][j]:
+                    if order_map[words[i][j]] > order_map[words[i + 1][j]]:
+                        return False
+                    break
+
+        return True
+
+
