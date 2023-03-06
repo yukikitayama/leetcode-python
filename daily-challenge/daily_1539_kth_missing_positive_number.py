346987b4341dde52ca967f9c30977e6873f9af30
Yuki Kitayama <yk2797@columbia.edu>
2023-03-06 06:28:35 -0800

    solved 1539 by iteration

---
+++ daily-challenge/daily_1539_kth_missing_positive_number.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+class Solution:
+    def findKthPositive(self, arr: List[int], k: int) -> int:
+
+        if k <= arr[0] - 1:
+            return k
+
+        k -= arr[0] - 1
+
+        for i in range(len(arr) - 1):
+
+            curr_missing = arr[i + 1] - arr[i] - 1
+
+            if k <= curr_missing:
+                return arr[i] + k
+
+            k -= curr_missing
+
+        return arr[-1] + k
+
+
+
