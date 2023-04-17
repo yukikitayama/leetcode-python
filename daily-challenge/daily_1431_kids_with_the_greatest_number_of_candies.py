a77cebbe8d547d5ae9ab010debf553339a5bf6f4
Yuki Kitayama <yk2797@columbia.edu>
2023-04-17 15:13:19 -0700

    solved 1431 by iteration

---
+++ daily-challenge/daily_1431_kids_with_the_greatest_number_of_candies.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+class Solution:
+    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+        max_ = max(candies)
+
+        ans = []
+        for i in range(len(candies)):
+            if candies[i] + extraCandies >= max_:
+                ans.append(True)
+            else:
+                ans.append(False)
+
+        return ans
