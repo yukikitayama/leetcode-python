cb5103aef3d76061d26f8cd3ad0c261f688ba83b
Yuki Kitayama <yk2797@columbia.edu>
2024-02-07 06:52:51 -0800

    math, hashmap

---
+++ company/facebook/python/202402/fb_560_subarray_sum_equals_k.py
@@ -0,0 +1,29 @@
+"""
+If cumulative sum up to i and j has difference of k
+  sum of elements between i and j is k
+
+when curr cumsum - prev cumsum = k
+  subarray between curr index and prev index has sum k
+  curr cumsum - k = prev cumsum
+"""
+
+
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        ans = 0
+
+        cumsum_to_count = collections.defaultdict(int)
+
+        # Empty subarray
+        cumsum = 0
+        cumsum_to_count[cumsum] = 1
+
+        for i in range(len(nums)):
+            cumsum += nums[i]
+
+            if cumsum - k in cumsum_to_count.keys():
+                ans += cumsum_to_count[cumsum - k]
+
+            cumsum_to_count[cumsum] += 1
+
+        return ans
