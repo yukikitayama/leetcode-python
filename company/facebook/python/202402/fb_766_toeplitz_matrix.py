0c0ecc7961528f30e364d128c3593984d13fb380
Yuki Kitayama <yk2797@columbia.edu>
2024-02-08 06:27:59 -0800

    iteration

---
+++ company/facebook/python/202402/fb_766_toeplitz_matrix.py
@@ -0,0 +1,22 @@
+"""
+Iterate every cell
+  when row != 0 and col != 0
+    if matrix[row][col] != matrix[row - 1][col - 1]
+      return False
+return True
+
+T: O(NM)
+S: O(1)
+"""
+
+from typing import List
+
+
+class Solution:
+    def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
+        for row in range(len(matrix)):
+            for col in range(len(matrix[0])):
+                if row != 0 and col != 0:
+                    if matrix[row][col] != matrix[row - 1][col - 1]:
+                        return False
+        return True
