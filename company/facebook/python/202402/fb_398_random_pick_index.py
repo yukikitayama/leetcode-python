7e6bacc7f7b624d3699b3a57021e1a934ccf1c6c
Yuki Kitayama <yk2797@columbia.edu>
2024-02-08 15:38:05 -0800

    random, array

---
+++ company/facebook/python/202402/fb_398_random_pick_index.py
@@ -0,0 +1,24 @@
+"""
+Hashmap
+  k: num
+  v: list of indices of nums
+
+Pick
+  random.choice from a particular list
+"""
+
+
+class Solution:
+
+    def __init__(self, nums: List[int]):
+        self.num_to_indices = collections.defaultdict(list)
+        for i in range(len(nums)):
+            self.num_to_indices[nums[i]].append(i)
+
+    def pick(self, target: int) -> int:
+        indices = self.num_to_indices[target]
+        return random.choice(indices)
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(nums)
+# param_1 = obj.pick(target)
