eb3333cbe6aa41ef3a872f247b12349c83e349aa
Yuki Kitayama <yk2797@columbia.edu>
2024-02-11 10:28:33 -0800

    add missing library

--- company/facebook/python/202402/fb_173_binary_search_tree_iterator.py
+++ company/facebook/python/202402/fb_173_binary_search_tree_iterator.py
@@ -12,13 +12,17 @@ Naive
     return number at the pointer index
 """
 
+from typing import Optional
+
 
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
 class BSTIterator:
 
     # def __init__(self, root: Optional[TreeNode]):
