1555cb0f53eb980fff1e4caa4f8390ae20ce9c13
Yuki Kitayama <yk2797@columbia.edu>
2024-02-20 23:07:41 -0800

    bfs, hashmap

--- company/facebook/python/202402/fb_314_binary_tree_vertical_order_traversal_2.py
+++ company/facebook/python/202402/fb_314_binary_tree_vertical_order_traversal_2.py
@@ -1,18 +1,23 @@
 """
-collect node values by hashmap
-  horizontal index to list of node values
+BFS
+  Order by the vertical can maintain by level order traversal and from left to right
+  horizontal index can be decided by the root as 0
+    -1 to left,
+    1 to right
+  hashmap
+    k: horizontal
+    v: list of vertically collected integers
 
-BFS and track depth and horizontal index
-  depth for order in list in list
-  index for indexing of a list
-
-7:19
+Get minimum of key
+  subtract the min from all the keys to adjust real array index
+    eg. -2 -> -2 --2 = 0, 2 -> 2 - 2 -> 0
 """
 
 from typing import Optional, List
 import collections
 
 
+# Definition for a binary tree node.
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
@@ -26,53 +31,29 @@ class Solution:
         if not root:
             return []
 
-        loc_to_values = collections.defaultdict(list)
-
+        loc_to_vals = collections.defaultdict(list)
         queue = collections.deque()
-        # (node, horizontal location index)
+        # [(node, horizontal location)]
         queue.append((root, 0))
 
         while queue:
 
             for _ in range(len(queue)):
 
-                curr_node, loc = queue.popleft()
-                curr_val = curr_node.val
+                node, loc = queue.popleft()
 
-                loc_to_values[loc].append(curr_val)
+                loc_to_vals[loc].append(node.val)
 
-                if curr_node.left:
-                    queue.append((curr_node.left, loc - 1))
-                if curr_node.right:
-                    queue.append((curr_node.right, loc + 1))
+                if node.left:
+                    queue.append((node.left, loc - 1))
+                if node.right:
+                    queue.append((node.right, loc + 1))
 
-        # print(loc_to_values)
-
-        # Wanna convert [-2, -1, 0, 1, 2] to [0, 1, 2, 3, 4]
-        # -2 - (-2) = 0
-        # -1 - (-2) = 1
-        # 0 - (-2) = 2
-        # 1 - (-2) = 3
-        # [0, 1, 2, 3], min: 0
-        # 0 - 0 = 0
-        # 1 - 0 = 1
-
-        min_horizontal_index = min(loc_to_values.keys())
-        horizontal_length = len(loc_to_values.keys())
-        ans = [None] * horizontal_length
-        for k, v in loc_to_values.items():
-            i = k - min_horizontal_index
+        min_loc = min(loc_to_vals.keys())
+        loc_length = len(loc_to_vals.keys())
+        ans = [None] * loc_length
+        for k, v in loc_to_vals.items():
+            i = k - min_loc
             ans[i] = v
 
         return ans
-
-
-if __name__ == "__main__":
-    root = TreeNode(3)
-    root.left = TreeNode(9)
-    root.right = TreeNode(8)
-    root.left.left = TreeNode(4)
-    root.left.right = TreeNode(0)
-    root.right.left = TreeNode(1)
-    root.right.right = TreeNode(7)
-    print(Solution().verticalOrder(root))
