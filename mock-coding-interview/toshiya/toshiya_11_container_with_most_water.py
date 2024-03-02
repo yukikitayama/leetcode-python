8a7fdf86f9ccff3befc774c946922761188f79c2
Yuki Kitayama <yk2797@columbia.edu>
2024-03-02 08:19:57 -0800

    math, two pointers

---
+++ mock-coding-interview/toshiya/toshiya_11_container_with_most_water.py
@@ -0,0 +1,26 @@
+from typing import List
+
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        ans = 0
+
+        left = 0
+        right = len(height) - 1
+
+        while left < right:
+
+            w = right - left
+            h = min(height[left], height[right])
+
+            area = w * h
+
+            ans = max(ans, area)
+
+            if height[left] < height[right]:
+                left += 1
+
+            else:
+                right -= 1
+
+        return ans
