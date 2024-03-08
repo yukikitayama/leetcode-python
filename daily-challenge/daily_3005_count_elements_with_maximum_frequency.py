f32014125e15a91dc04eeef7b00c2b91ce5aea06
yukikitayama <yk2797@columbia.edu>
2024-03-08 06:42:54 -0800

    hashmap

---
+++ daily-challenge/daily_3005_count_elements_with_maximum_frequency.py
@@ -0,0 +1,15 @@
+from typing import List
+import collections
+
+
+class Solution:
+    def maxFrequencyElements(self, nums: List[int]) -> int:
+        counter = collections.Counter(nums)
+        max_freq = max(counter.values())
+        ans = 0
+
+        for k, v in counter.items():
+            if v == max_freq:
+                ans += v
+
+        return ans
