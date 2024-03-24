e4ae18a6063d85a8685f50d17739a958ae8208e3
yukikitayama <yk2797@columbia.edu>
2024-03-23 21:41:14 -0700

    lazy update

--- contest/weekly/2024-03-23/contest_100258_most_frequent_ids.py
+++ contest/weekly/2024-03-23/contest_100258_most_frequent_ids.py
@@ -9,6 +9,36 @@ class Solution:
         heapq.heapify(max_heap)
         id_to_freq = collections.Counter()
 
+        ans = [0] * len(nums)
+
+        for i in range(len(nums)):
+
+            n = nums[i]
+            f = freq[i]
+
+            id_to_freq[n] += f
+
+            # This make heap contains duplicated IDs with different freq
+            # but it's okay because we only care heap top freq and ID
+            heapq.heappush(max_heap, (-id_to_freq[n], n))
+
+            # This pops outdate large freq and ID
+            # e.g. previously freq was 2, but currently freq is 0, 2 at the heap top, so discard it
+            while max_heap and -max_heap[0][0] != id_to_freq[max_heap[0][1]]:
+                heapq.heappop(max_heap)
+
+            if max_heap:
+                ans[i] = -max_heap[0][0]
+
+            # print(i, max_heap)
+
+        return ans
+
+    def mostFrequentIDs1(self, nums: List[int], freq: List[int]) -> List[int]:
+        max_heap = []
+        heapq.heapify(max_heap)
+        id_to_freq = collections.Counter()
+
         ans = []
 
         for i in range(len(nums)):
