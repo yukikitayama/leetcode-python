88c3bac00b4e380d1783c0852c056f910cd2c11f
yukikitayama <yk2797@columbia.edu>
2024-04-12 22:41:18 -0700

    modulo, left, case when, postgresql

---
+++ mock-coding-interview/toshiya/toshiya_1873_calculate_special_bonus.sql
@@ -0,0 +1,12 @@
+-- Write your PostgreSQL query statement below
+select
+  employee_id,
+  case
+    when employee_id % 2 != 0 and left(name, 1) != 'M' then salary
+    else 0
+  end as bonus
+from
+  employees
+order by
+  1
+;
