a073999b509606067a03bbbdc643670b5e204a3a
yukikitayama <yk2797@columbia.edu>
2024-08-25 21:55:29 -0700

    hashmap

---
+++ mock-coding-interview/satoshi/satoshi_383_ransom_note.py
@@ -0,0 +1,6 @@
+import collections
+
+
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        return collections.Counter(ransomNote) <= collections.Counter(magazine)
