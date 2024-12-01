610849bceb62de6ac27cd1bde5854fbacfc3e3b2
Yuki Kitayama <yk2797@columbia.edu>
2024-12-01 09:09:49 -0800

    hashmap

---
+++ daily-challenge/daily_1346_check_if_n_and_its_double_exist.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+class Solution:
+    def checkIfExist(self, arr: List[int]) -> bool:
+        for i in range(len(arr)):
+            for j in range(i + 1, len(arr)):
+                if arr[i] == (2 * arr[j]) or arr[j] == (2 * arr[i]):
+                    return True
+        return False
