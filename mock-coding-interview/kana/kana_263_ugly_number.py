8a70b03d324d80e06f532a17b90e4d1dc71e907f
Yuki Kitayama <yk2797@columbia.edu>
2025-03-23 10:23:32 -0700

    math, understand problem

---
+++ mock-coding-interview/kana/kana_263_ugly_number.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isUgly(self, n: int) -> bool:
+        if n <= 0:
+            return False
+
+        while n % 2 == 0 or n % 3 == 0 or n % 5 == 0:
+            if n % 2 == 0:
+                n /= 2
+            elif n % 3 == 0:
+                n /= 3
+            elif n % 5 == 0:
+                n /= 5
+            else:
+                return False
+
+        if n == 1:
+            return True
+        else:
+            return False
