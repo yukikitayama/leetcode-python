cab89e38e20bc561263f896b6f02a0d0e4ac4516
Yuki Kitayama <yk2797@columbia.edu>
2025-05-10 10:20:29 -0700

    simulation

---
+++ daily-challenge/daily_2918_minimum_equal_sum_of_two_arrays_after_replacing_zeros.py
@@ -0,0 +1,28 @@
+"""
+sum
+count 0
+"""
+
+from typing import List
+
+class Solution:
+    def minSum(self, nums1: List[int], nums2: List[int]) -> int:
+        zero1 = zero2 = 0
+        sum1 = sum2 = 0
+
+        for num1 in nums1:
+            sum1 += num1
+            if num1 == 0:
+                zero1 += 1
+                sum1 += 1
+
+        for num2 in nums2:
+            sum2 += num2
+            if num2 == 0:
+                zero2 += 1
+                sum2 += 1
+
+        if (zero1 == 0 and sum1 < sum2) or (zero2 == 0 and sum2 < sum1):
+            return -1
+        else:
+            return max(sum1, sum2)
