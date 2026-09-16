35730706504f18c68c4830858b6d4c426de087b7
Yuki Kitayama <yk2797@columbia.edu>
2026-09-16 09:09:33 -0700

    greedy

---
+++ company/tiktok/tiktok_2640_find_the_score_of_all_prefixes_of_an_array.py
@@ -0,0 +1,22 @@
+"""
+max_so_far
+curr_sum
+"""
+
+
+class Solution:
+    def findPrefixScore(self, nums: List[int]) -> List[int]:
+        ans = []
+
+        max_so_far = 0
+        curr_sum = 0
+
+        for i in range(len(nums)):
+
+            max_so_far = max(max_so_far, nums[i])
+            curr_num = nums[i] + max_so_far
+            curr_sum += curr_num
+
+            ans.append(curr_sum)
+
+        return ans
